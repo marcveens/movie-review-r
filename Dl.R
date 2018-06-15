14dad14969552345c14e3876070d159c8c063d08
Marc Veens <Marc.Veens@macaw.nl>
2018-06-15 13:27:44 +0200

    Update

--- Dl.R
+++ Dl.R
@@ -16,28 +16,34 @@ library(dplyr)
 library(caret)
 library(mongolite)
 
+## Get all data from Mongo
 con=mongo(collection="reviews",db="movies")
-
 df <-con$find("{}")
 df_count <- nrow(df)
 
+## The seed makes the sample function predictable. The results will stay the same
 set.seed(1)
-df <- df[sample(nrow(df)), ]
-df <- df[sample(nrow(df)), ]
 
-df$sentiment <- as.factor(df$sentiment)
-
-corpus <- Corpus(VectorSource(df$text))
-
-corpus.clean <- corpus %>%
+## Clean the corpus
+clean_corpus <- function(corpus) {
+  corpus %>%
   tm_map(content_transformer(tolower)) %>% 
   tm_map(removePunctuation) %>%
   tm_map(removeNumbers) %>%
   tm_map(removeWords, stopwords(kind="en")) %>%
   tm_map(stripWhitespace)
+}
+
+## Randomize all results
+df <- df[sample(nrow(df)), ]
+
+corpus <- Corpus(VectorSource(df$text))
+
+corpus.clean <- clean_corpus(corpus)
 
 dtm <- DocumentTermMatrix(corpus.clean)
 
+## Used for the statistics later on
 df.train <- df[1:ceiling(df_count / 2),]
 df.test <- df[(ceiling(df_count / 2) + 1):df_count,]
 
@@ -47,42 +53,10 @@ dtm.test <- dtm[(ceiling(df_count / 2) + 1):df_count,]
 corpus.clean.train <- corpus.clean[1:ceiling(df_count / 2)]
 corpus.clean.test <- corpus.clean[(ceiling(df_count / 2) + 1):df_count]
 
-dim(dtm.train)
-
-## Custom test set
-doc_id <- c(10000,10001,10002,10003,10004,10005,10006)
-text <- c("very good", "very bad", "nice", "like", "hate", "love", "I really like this movie. Awesome")
-sentiment <- c(1, 0, 1, 1, 0, 1, 1)
-sentiment <- as.integer(sentiment)
-df.test <- data.frame(doc_id, text, sentiment)
-test_corpus <- Corpus(VectorSource(df.test$text))
-test_corpus.clean <- test_corpus %>%
-  tm_map(content_transformer(tolower)) %>% 
-  tm_map(removePunctuation) %>%
-  tm_map(removeNumbers) %>%
-  tm_map(removeWords, stopwords(kind="en")) %>%
-  tm_map(stripWhitespace)
-dtm.test <- DocumentTermMatrix(test_corpus.clean)
-corpus.clean.test <- test_corpus.clean[]
-
-
-
-
-
 fivefreq <- findFreqTerms(dtm.train, 5)
-length((fivefreq))
-## [1] 12144
-
-# Use only 5 most frequent words (fivefreq) to build the DTM
 
 dtm.train.nb <- DocumentTermMatrix(corpus.clean.train, control=list(dictionary = fivefreq))
-
-dim(dtm.train.nb)
-## [1]  1500 12144
-
 dtm.test.nb <- DocumentTermMatrix(corpus.clean.test, control=list(dictionary = fivefreq))
-
-dim(dtm.train.nb)
 
 convert_count <- function(x) {
   y <- ifelse(x > 0, 1,0)
@@ -93,11 +67,20 @@ convert_count <- function(x) {
 trainNB <- apply(dtm.train.nb, 2, convert_count)
 testNB <- apply(dtm.test.nb, 2, convert_count)
 
-system.time( classifier <- naiveBayes(trainNB, df.train$sentiment, laplace = 1) )
-system.time( pred <- predict(classifier, newdata=testNB) )
+classifier <- naiveBayes(trainNB, df.train$sentiment, laplace = 1)
+pred <- predict(classifier, newdata=testNB)
 
 table("Predictions"= pred,  "Actual" = df.test$sentiment )
 
 conf.mat <- confusionMatrix(pred, df.test$sentiment)
 
 conf.mat
+
+custom_reviews <- c("very good", "very bad", "nice", "ingredients : possessed plastic dolls in love , plastic dolls having sex starring : jennifer tilly , voice of brad dourif , katherine heigl , nick stabile , john ritter synopsis : this is the fourth film in the chucky series , which debuted in the late 1980s . basically , chucky is a plastic doll that can walk and talk because it is possessed by the spirit of a slain murderer . in bride of chucky , chucky's longtime girlfriend tiffany ( jennifer tilly ) dies and her spirit inhabits a female plastic doll through voodoo . dolls tiffany and chucky get married and embark on a quest to reach a cemetery in new jersey , where a mystical gem might enable them to be humans . the dolls stow away in the back of a vehicle driven by a newly eloped couple , so in the side plot , the couple suspects each other of being a murderer . opinion : bride of chucky is an attempt at horror with humor , but doesn't succeed . somehow , chucky moaning about mid life crisis , and how he should have gotten married does not make for a very scary chucky . and tiffany harping about mid life crisis , and how she should have gotten married does not make for a very scary tiffany . the suspenseless bride of chucky relies mostly on jennifer tilly's cleavage to keep attention during the first half , and on occasional puns to keep attention during the second half . the best that can be said about bride of chucky is that it is sarcastic .", "hate", "love", "I really like this movie. Awesome")
+custom_corpus <- Corpus(VectorSource(custom_reviews))
+custom_corpus <- clean_corpus(custom_corpus)
+custom_dtm <- DocumentTermMatrix(custom_corpus, control=list(dictionary = fivefreq))
+custom_dtm <- apply(custom_dtm, 2, convert_count)
+custom_pred <- predict(classifier, newdata=custom_dtm)
+custom_pred
+table("pred"=custom_pred)
